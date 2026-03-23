e90a1dbcde3c112350471912ac7ca9cc66a4fad4
HoaiAnh <abc@gmail.com>
2026-03-24 06:14:26 +0700

    Add salsa.c implementing Salsa20 functions

---
+++ salsa.c
@@ -0,0 +1,24 @@
+#include <stdint.h>
+#include <stddef.h>
+#include "salsa20.h"
+static void s20_rowround(uint32_t y[static 16])
+{
+  s20_quarterround(&y[0], &y[1], &y[2], &y[3]);
+  s20_quarterround(&y[5], &y[6], &y[7], &y[4]);
+  s20_quarterround(&y[10], &y[11], &y[8], &y[9]);
+  s20_quarterround(&y[15], &y[12], &y[13], &y[14]);
+}
+
+static void s20_columnround(uint32_t x[static 16])
+{
+  s20_quarterround(&x[0], &x[4], &x[8], &x[12]);
+  s20_quarterround(&x[5], &x[9], &x[13], &x[1]);
+  s20_quarterround(&x[10], &x[14], &x[2], &x[6]);
+  s20_quarterround(&x[15], &x[3], &x[7], &x[11]);
+}
+
+static void s20_doubleround(uint32_t x[static 16])
+{
+  s20_columnround(x);
+  s20_rowround(x);
+}
